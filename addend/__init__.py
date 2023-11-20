532b6bdede9d18b0fa2d8e98d286bacd0d1eab68
Klaus <klaus-szyska@comcast.net>
2023-11-20 16:43:38 -0500

    Add files via upload
    Initial pypi release 'addend 1.14' 11/20/2023
    no 'python' or '.py' extension needed to execute
    run as: addend --help

---
+++ addend/__init__.py
@@ -0,0 +1,1 @@
+from .addend import main
---
+++ addend/command_line.py
@@ -0,0 +1,8 @@
+import addend
+
+def main():
+    addend.main()
+
+# to launch addend from command line
+# see https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html
+# #the-console-scripts-entry-point
